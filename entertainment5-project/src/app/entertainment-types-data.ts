82c6ac452b7ebeab32127829c29e6b624b775947
Courtney Tyler <courtneytyler7@gmail.com>
2019-07-12 18:48:39 -0400

    created angular project, added data file

---
+++ entertainment5-project/src/app/entertainment-types-data.ts
@@ -0,0 +1,23 @@
+export const entertainmentTypesArray = [
+  {
+    name: "Video Streaming Services",
+    description:
+      "Streaming services such as Netflix and Amazon Prime are one of the top forms of entertainment in 2019.",
+    fact:
+      "The global video streaming market is estimated to be worth over $125 billion USD by 2025. (Source: MarketWatch)"
+  },
+  {
+    name: "Gaming",
+    description:
+      "Playing video games on various devices such as consoles and mobile phones.",
+    fact:
+      "86% say they’ve gamed on at least one device in the past month. (Source: GlobalWebIndex)"
+  },
+  {
+    name: "Sports",
+    description:
+      "Watching sporting events or playing sports such as soccer, basketball, or football",
+    fact:
+      "The top 3 most popular sports in the world in 2019 are soccer, cricket, and basketball. (Source: SportsShow.net)"
+  }
+];
